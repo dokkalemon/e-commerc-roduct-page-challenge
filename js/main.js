1a2c6b1efae5f16cb47accd4100f6017ffc83692
dokkalemon <dottori.roberto@gmail..com>
2021-11-28 22:00:14 +0100

    add burger menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log('ciao');
+/* Menu Mobile Active */
+const burger = document.querySelector('.burger img');
+const nav = document.querySelector('nav');
+const cross = document.querySelector('.cross img');
+
+
+burger.addEventListener('click', function() {
+    nav.classList.add('active')
+})
+
+cross.addEventListener('click', function() {
+    nav.classList.remove('active')
+})
